d38fba4639c2bc48be6c7081816907ea684ed85e
Yasin-Ozdemir <yasinozdemirgs37@gmail.com>
2025-01-29 16:17:04 +0300

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Yasin Özdemir on 29.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
